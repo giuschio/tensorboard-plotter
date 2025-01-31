af959f257dbcf6dd5ae003f35fcdc28aad5321bc
Giulio Schiavi <giuschio98@gmail.com>
2025-01-31 14:14:54 +0100

    Project structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tensorboard-plotter
+
+## Installation
+
+```bash
+pip install git+https://github.com/giuschio/tensorboard-plotter
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="tensorboard-plotter",   # The name of your PyPI package
+    version="0.1.0",
+    description="A tool to plot aggregate TensorBoard logs from the command line.",
+    author="Giulio Schiavi",
+    url="https://github.com/giuschio/tensorboard-plotter",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            # console_script_name = package.module:function
+            "tensorboard-plotter = tensorboard_plotter.plot:main"
+        ],
+    },
+    # If you have data files (like templates), you might need:
+    # include_package_data=True,
+    # package_data={"tensorboard_plotter": ["some_data_file"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
